98eb55d0441473f1e9cf0ac5bc4c13831bd88fb8
huanyushi <1174601344@qq.com>
2020-05-03 21:51:33 +0800

    tf2.0重构ing

--- data.py
+++ data.py
@@ -1,3 +1,4 @@
+import os
 import numpy as np
 from scipy.io import loadmat
 from sklearn.model_selection import train_test_split
@@ -5,8 +6,10 @@ import tensorflow as tf
 from tensorflow.keras.layers import Dense, Flatten, Conv2D
 from tensorflow.keras import Model
 from tensorflow.keras.optimizers import Adam
-from tensorflow.keras.losses import CategoricalCrossentropy
+from tensorflow.keras.losses import categorical_crossentropy
 from tensorflow.keras.utils import to_categorical
+
+tf.keras.backend.set_floatx('float64')
 
 
 def split_data(datas, lables, p_train, p_test, p_val=0):
@@ -21,7 +24,9 @@ def split_data(datas, lables, p_train, p_test, p_val=0):
 
 # 从.mat文件加载Indian_pines数据集
 def load_data():
-    Indian_pines_corrected = loadmat('databases/Indian_pines_corrected.mat')['indian_pines_corrected'].astype('float64')
+    Indian_pines_corrected = loadmat('databases/Indian_pines_corrected.mat')['indian_pines_corrected']
+    Indian_pines_corrected=Indian_pines_corrected/Indian_pines_corrected.max()
+    Indian_pines_corrected.astype('float64')
     Indian_pines_gt = loadmat('databases/Indian_pines_gt.mat')['indian_pines_gt'].astype('float64')
     return Indian_pines_corrected, Indian_pines_gt
 
@@ -38,9 +43,9 @@ def get_data():
     # Indian_pines.shape is (21025, 200),Indian_pines_gt.shape is (21025, 1)
     # print(f"Indian_pines.shape is {Indian_pines.shape},Indian_pines_gt.shape is {Indian_pines_gt.shape}")
 
-    Indian_pines_gt = to_categorical(Indian_pines_gt, np.unique(Indian_pines_gt).shape[0])
+    # Indian_pines_gt = to_categorical(Indian_pines_gt, np.unique(Indian_pines_gt).shape[0])
     # 划分数据集为训练集、测试集、验证集 (6:3:1)
-    return split_data(Indian_pines, Indian_pines_gt, 6, 3, 1)
+    return split_data(Indian_pines, Indian_pines_gt, 6, 2, 2)
 
 
 train_x, train_y, test_x, test_y, val_x, val_y = get_data()
@@ -49,10 +54,10 @@ train_x, train_y, test_x, test_y, val_x, val_y = get_data()
 class MyModel(Model):
     def __init__(self):
         super(MyModel, self).__init__()
-        self.d1 = Dense(256, activation='relu')
-        self.d2 = Dense(256, activation='relu')
-        self.d3 = Dense(256, activation='relu')
-        self.d4 = Dense(17, activation='relu')
+        self.d1 = Dense(1024, activation='relu')
+        self.d2 = Dense(1024, activation='relu')
+        self.d3 = Dense(1024, activation='relu')
+        self.d4 = Dense(17, activation='sigmoid')
 
     def call(self, x):
         x = self.d1(x)
@@ -62,9 +67,10 @@ class MyModel(Model):
 
 
 model = MyModel()
-
-optimizer = Adam()
-loss_object = CategoricalCrossentropy()
+model.build(input_shape=(None, 200))
+# print(model.summary())
+optimizer = tf.keras.optimizers.Adam(learning_rate=0.0001)
+loss_object = tf.keras.losses.SparseCategoricalCrossentropy()
 
 train_loss = tf.keras.metrics.Mean(name='train_loss')
 train_accuracy = tf.keras.metrics.SparseCategoricalAccuracy(name='train_accuracy')
@@ -84,13 +90,15 @@ def train_step(images, labels):
     train_loss(loss)
     train_accuracy(labels, predictions)
 
-    @tf.function
-    def test_step(images, labels):
-        predictions = model(images)
-        t_loss = loss_object(labels, predictions)
 
-        test_loss(t_loss)
-        test_accuracy(labels, predictions)
+@tf.function
+def test_step(images, labels):
+    predictions = model(images)
+    t_loss = loss_object(labels, predictions)
+
+    test_loss(t_loss)
+    test_accuracy(labels, predictions)
+
 
 EPOCHS = 10000
 for epoch in range(EPOCHS):
@@ -100,11 +108,8 @@ for epoch in range(EPOCHS):
     test_loss.reset_states()
     test_accuracy.reset_states()
 
-    for i in range(len(train_x)):
-        train_step(train_x[i][:,np.newaxis], train_y[i][:,np.newaxis])
-
-    for i in range(len(test_x)):
-        test_step(test_x[i][:,np.newaxis], test_y[i][:,np.newaxis])
+    train_step(train_x, train_y)
+    test_step(test_x, test_y)
 
     template = 'Epoch {}, Loss: {}, Accuracy: {}, Test Loss: {}, Test Accuracy: {}'
     print(template.format(epoch + 1,
@@ -113,12 +118,9 @@ for epoch in range(EPOCHS):
                           test_loss.result(),
                           test_accuracy.result() * 100))
     if not epoch:
-        best_val = test_accuracy
-        print(f"acc提高了，保存模型{epoch}--{train_accuracy}---{test_accuracy}.h5")
-        model.save(f"{epoch}--{train_accuracy}---{test_accuracy}.h5")
+        best_val = test_accuracy.result()
+
+    if test_accuracy.result() > best_val:
+        print(f"acc提高了，保存模型")
     else:
-        if test_accuracy>best_val:
-            model.save(f"{epoch}--{train_accuracy}---{test_accuracy}.h5")
-            print(f"acc提高了，保存模型{epoch}--{train_accuracy}---{test_accuracy}.h5")
-        else:
-            print("acc没有提高")
+        print("acc没有提高")
